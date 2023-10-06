d9a2abfaea03aea40c20fe1faf10c958b9c18748
alexandre <alexandresouzabertunes1312@gmail.com>
2023-10-05 23:34:13 -0300

    Adicionando a model e Controller de Usuario

--- Eplist/EPIlist/Controllers/UsuarioController.cs
+++ Eplist/EPIlist/Controllers/UsuarioController.cs
@@ -1,5 +1,102 @@
+using EPIlist.Models;
+using Microsoft.AspNetCore.Mvc;
+using Namespace.Data;
+
 namespace EPIlist.Controllers;
-public class UsuarioController
+
+[ApiController]
+[Route("Eplist/Usuario")]
+public class UsuarioController : ControllerBase
 {
 
+    private readonly AppDataContext _ctx;
+
+    public UsuarioController(AppDataContext ctx) => _ctx = ctx;
+
+    [HttpGet]
+    [Route("listar")]
+
+    public IActionResult listar(){
+        try
+        {
+            List<Usuario> usuarios = _ctx.Usuarios.ToList();
+            return usuarios.Count ==0 ? NotFound() : Ok(usuarios);
+
+        }catch(Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    [HttpGet]
+    [Route("{id}")]
+
+    public IActionResult UsuarioId(int id)
+    {
+        Usuario usuario = _ctx.Usuarios.FirstOrDefault(e => e.UsuarioID == id);
+        return usuario == null ? NotFound(""): Ok(usuario);
+    }
+
+    [HttpPost]
+    [Route("cadastrar")]
+
+    public IActionResult Cadastrar([FromBody] Usuario usuario)
+    {
+        try
+        {
+            _ctx.Add(usuario);
+            _ctx.SaveChanges();
+            return Created("", usuario);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+
+    }
+
+    [HttpPut]
+    [Route("atualizar/{id}")]
+
+    public IActionResult AtualizarUsuario(int id, [FromBody] Usuario usuarioAtualizado)
+    {
+        if (usuarioAtualizado == null)
+        {
+            return BadRequest("Dados do usuario invalido");
+        }
+
+        Usuario usuario = _ctx.Usuarios.FirstOrDefault(e => e.UsuarioID == id);
+        
+        if(usuario == null)
+        {
+            return NotFound("Usuario não encontrado");
+        }
+
+        usuario.Nome = usuarioAtualizado.Nome;
+        usuario.CPF = usuarioAtualizado.CPF;
+        usuario.Telefone = usuarioAtualizado.Telefone;
+        usuario.Cargo = usuarioAtualizado.Cargo;
+        usuario.Email = usuarioAtualizado.Email;
+
+        _ctx.Update(usuario);
+        _ctx.SaveChanges();
+        return Ok(usuario);
+        
+    }
+
+    [HttpDelete]
+    [Route("Deletar/{id}")]
+    public IActionResult DeletarUsuario(int id)
+    {
+        Usuario usuarioExistente = _ctx.Usuarios.FirstOrDefault(e => e.UsuarioID == id);
+
+        if(usuarioExistente == null)
+        {
+            return NotFound("Usuario não encontrado");
+        }
+
+        _ctx.Usuarios.Remove(usuarioExistente);
+        _ctx.SaveChanges();
+        return NoContent();
+    }
 }
---
+++ Eplist/EPIlist/Migrations/20231006023042_adcModels.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPIlist.Migrations
+{
+    public partial class adcModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Epis",
+                columns: table => new
+                {
+                    EpiID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false),
+                    C_A = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantidade = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Epis", x => x.EpiID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipes",
+                columns: table => new
+                {
+                    EquipeID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NomeEquipe = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipes", x => x.EquipeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Unidades",
+                columns: table => new
+                {
+                    UnidadeID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Unidades", x => x.UnidadeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    UsuarioID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefone = table.Column<string>(type: "TEXT", nullable: false),
+                    Senha = table.Column<string>(type: "TEXT", nullable: false),
+                    CPF = table.Column<string>(type: "TEXT", nullable: false),
+                    Cargo = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.UsuarioID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Epis");
+
+            migrationBuilder.DropTable(
+                name: "Equipes");
+
+            migrationBuilder.DropTable(
+                name: "Unidades");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
